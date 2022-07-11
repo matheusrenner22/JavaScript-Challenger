70c7b24d8dd6ca49f5ebb8f005360ee4d494198f
Matheus Rossi <matheus.rossi@di2win.com>
2022-07-11 17:00:26 -0300

    added new exercises

---
+++ fundamentos/6-get-last-n-characters-of-string.js
@@ -0,0 +1,12 @@
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+
+function myFunction(str) {
+  return str.slice(-3);
+}
+
+// Test Cases:
+console.log(myFunction('abcdefg'));
+console.log(myFunction('1234'));
+console.log(myFunction('fgedcba'));
---
+++ fundamentos/9-remove-last-n-characters-of-string.js
@@ -0,0 +1,12 @@
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+
+function myFunction(a) {
+  return a.slice(0, -3);
+}
+
+// Tests Cases:
+console.log(myFunction('abcdefg'));
+console.log(myFunction('1234'));
+console.log(myFunction('fgedcba'));
